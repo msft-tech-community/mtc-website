abbe09db9a6c328fcb9fc59a5826606921b34f5f
FortxArcader <shlokbhardwaj1@gmail.com>
2025-07-19 19:08:05 +0530

    Feed : added new components for the website
    Team Member Card component creation
    Footer Component Creation
    Added Events Slide Show Etc..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import HeroSection from "../components/HeroSection.jsx";
 import HighlightList from "../components/HighlightList.jsx";
+import EventsSlideshow from "../components/EventsSlideshow.jsx"; // <-- Add this line
+import Footer from "../components/Footer.jsx";
 
+import TeamMemberCard from "../components/TeamMemberCard.jsx";
 export default function Home() {
     return (
         <>
             <HeroSection />
             <HighlightList />
+            <TeamMemberCard />
+            <EventsSlideshow />
+            <Footer />
         </>
     );
 }
